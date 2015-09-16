28e57d2644581554c2cbdb9e4a4f628b4239ac86
dtrinh888 <dtrinh888@gmail.com>
2015-09-15 23:49:44 -0700

    Added Select/Deselect checkbox to shopping list

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 
     $(".add-btn").click(function (){
 		var addItem = $('#item-add').val();
-		$(".check-list").append('<li><input type="checkbox">' + addItem + '</li>');
+		$(".check-list").append('<li ><input type="checkbox" class="check-box">' + addItem + '</li>');
+	});
+
+	$('#select-all').change(function(){
+		$('.check-box').prop('checked', $(this).prop('checked'));
 	});
 
 	$(".rem-btn").click(function(){
-		$(".check-list input:checked").parent().remove();
+		$(".check-list input:checked").not('#select-all').parent().remove();
 	});
 
 });
